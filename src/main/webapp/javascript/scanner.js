ada591b14fbc567fcc1d1171f300f7c46df30cef
Mohamed Sheriffdeen <m.scelus.s@gmail.com>
2011-05-17 14:53:29 -0700

    moved db stuff into scanner.js fixed ajax page navigation by manually handling it

--- src/main/webapp/javascript/scanner.js
+++ src/main/webapp/javascript/scanner.js
@@ -20,8 +20,27 @@ function clearCache() {
 }
 
 function init() {
+    var db = window.openDatabase("ofilestorage", "1.0", "Ovewaitea Scanner Settings", 1024, null);
+    if (!db) {
+        alert("Failed to connect to database.");
+    }
+    createTable(db);
+    
+    $("#form-login").submit(function() {
+        $.mobile.changePage($("#results"));
+        insertURL(db, "http://tomnightingale.com");
+        return false;
+    });
+
+    // Perform actions when the Login page is first created.
+    $('#login').live('pagecreate', function(event) {
+        console.log("Login page created.");
+    });
+    
     // Perform actions when the results page is first created.
     $('#results').live('pagecreate', function(event){
+        console.log("Results page created.");
+        
         // Load scanner right away.
         scanBarcode();
 
@@ -49,4 +68,46 @@ function scanBarcode() {
         function(error) {
             alert('Error: ' + error);
         });
+}
+
+/*********************************************************************
+ * Database stuff
+ ********************************************************************/
+function createTable(db) {
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql(
+        'CREATE TABLE serverLoc(url VARCHAR(5) PRIMARY KEY, urlValue VARCHAR(30))', [],
+        function() {
+          console.log("Create YAY");
+        },
+        function(transaction, error) {
+          console.log('Create FAIL ' + error.message);
+        }
+      );
+      
+    }
+  );
+}
+
+function insertURL(db, newUrl) {
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql('INSERT INTO serverLoc (urlValue) VALUES (?,?) WHERE ...', ["main",newUrl],
+        function() {
+          console.log("Insert YAY");
+        },
+        function(transaction, error) {
+          console.log('Insert FAIL ' + error.message);
+        });
+    }
+  );
+}
+
+function readURL(db) {
+  db.transaction(
+    function(transaction) {
+      transaction.executeSql('SELECT * FROM serverLoc', null, null);
+    }
+  );
 }
