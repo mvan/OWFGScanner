64dbd06bc3a612c89ede09d2ec3ddeffbb8babbe
Tom Nightingale <tom@tnightingale.com>
2011-05-19 19:10:12 -0700

    it works!!!!

--- OWFGScannerOS5/src/main/java/WebServiceExtension/com/owfg/webservice/Client.java
+++ OWFGScannerOS5/src/main/java/WebServiceExtension/com/owfg/webservice/Client.java
@@ -16,13 +16,13 @@ public final class Client extends ScriptableFunction {
 
     public Object invoke(Object obj, Object[] args) throws Exception {
         EventLogger.register(GUID, APP_NAME, EventLogger.VIEWER_STRING);
-        Logger.logInformationEvent("Client(): invoke");
+        Logger.logErrorEvent("Client.invoke(): invoke");
 
         String fnName = (String) args[5];
         ScriptableFunction success = (ScriptableFunction) args[0];
         ScriptableFunction error = (ScriptableFunction) args[1];
         if (ws == null) {
-            Logger.logInformationEvent("Client.invoke: create WS");
+            Logger.logErrorEvent("Client.invoke(): create WS");
             ws = new WebService((String) args[2], (String) args[3], (String) args[4]);
         }
         if (ws.getAddress().equals((String) args[2]) == false) {
@@ -37,23 +37,23 @@ public final class Client extends ScriptableFunction {
 
         try {
             if (fnName.equals(GET_INFO)) {
-                Logger.logInformationEvent("Client.invoke: get Info");
+                Logger.logErrorEvent("Client.invoke(): get Info");
                 ws.getInfo(success, error, (String) args[6]);
             }
             if (fnName.equals(GET_BANNERS)) {
-                Logger.logInformationEvent("Client.invoke: get banners");
+                Logger.logErrorEvent("Client.invoke(): get banners");
                 ws.getBanners(success, error);
             }
             if (fnName.equals(GET_STORES)) {
-                Logger.logInformationEvent("Client.invoke: get stores");
+                Logger.logErrorEvent("Client.invoke(): get stores");
                 ws.getStores(success, error);
             }
             if (fnName.equals(SET_STORE)) {
-                Logger.logInformationEvent("Client.invoke: get stores");
+                Logger.logErrorEvent("Client.invoke(): get stores");
                 ws.setStore(success, error, (Long) args[6]);
             }
         } catch (Exception e) {
-            Logger.logErrorEvent("Exception: Client.Invoke(); " + e.getMessage());
+            Logger.logErrorEvent("Exception: Client.Invoke(); (" + e + ") " + e.getMessage());
         }
         return UNDEFINED;
     }
